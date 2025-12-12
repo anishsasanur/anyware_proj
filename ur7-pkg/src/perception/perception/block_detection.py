af49e18d0b2ec1e20fa848ed6a54217d59657afa
Josh Zhang <joshzhang@berkeley.edu>
2025-12-11 23:59:56 -0800

    refactored

--- ur7-pkg/src/perception/perception/block_detection.py
+++ ur7-pkg/src/perception/perception/block_detection.py
@@ -16,41 +16,41 @@ class BlockDetection(Node):
     def __init__(self):
         super().__init__("block_detection")
 
-        # Camera intrinsics
+        # Intrinsics
         self.fx = self.fy = self.cx = self.cy = None
         self.depth_scale = 0.001
 
-        # ROS
+        # State vars
         self.bridge = CvBridge()
         self.depth_image = None
         self.color_image = None
 
+        # Period in s
+        self.detection_period = 2.0
+        self.create_timer(self.detection_period, self.block_detection)
+
+        # Subscribers
         self.create_subscription(
             CameraInfo,
             "/camera/camera/color/camera_info",
             self.camera_info_callback,
             1,
         )
-
         self.create_subscription(
             Image,
             "/camera/camera/aligned_depth_to_color/image_raw",
             self.depth_image_callback,
             1,
         )
-
         self.create_subscription(
             Image,
             "/camera/camera/color/image_rect_raw",
             self.color_image_callback,
             1,
         )
-
-        self.marker_pub = self.create_publisher(MarkerArray, "/block_centers", 10)
-        self.mask_pub = self.create_publisher(Image, "/binary_masks", 10)
-
-        self.timer_period = 2.0
-        self.create_timer(self.timer_period, self.process)
+        # Publishers
+        self.block_centers_pub = self.create_publisher(MarkerArray, "/block_centers", 10)
+        self.binary_masks_pub  = self.create_publisher(Image, "/binary_masks", 10)
 
 
     def camera_info_callback(self, msg: CameraInfo):
@@ -66,38 +66,36 @@ class BlockDetection(Node):
         self.color_image = self.bridge.imgmsg_to_cv2(msg, "bgr8")
 
 
-    def process(self):
-        if (
-            self.depth_image is None
+    def block_detection(self):
+        if (self.depth_image is None
             or self.color_image is None
             or self.fx is None
-        ):
-            return
+        ): return
 
-        # --- Send image to SAM ---
+        # Input data for SAM
         _, jpg = cv2.imencode(".jpg", self.color_image)
         files = {"image": ("image.jpg", io.BytesIO(jpg), "image/jpeg")}
         data = {"prompt": "square cube"}
 
+        # Output data of SAM
         response = requests.post(SAM_URL, files=files, data=data)
         if response.status_code != 200:
-            self.get_logger().error("SAM request failed")
+            self.get_logger().error(f"SAM request failed: {response}")
             return
-
         sam_data = response.json()
 
+        # All centers, masks
         block_centers = []
-        all_masks = np.zeros_like(self.color_image)
+        masks_stacked = np.zeros_like(self.color_image)
 
         for mask in sam_data["masks"]:
-            # Decode mask
+            # Get binary mask
             mask_bytes = base64.b64decode(mask["mask_base64"])
-            mask_np = np.frombuffer(mask_bytes, np.uint8)
-            mask_img = cv2.imdecode(mask_np, cv2.IMREAD_GRAYSCALE)
+            mask_numpy = np.frombuffer(mask_bytes, np.uint8)
+            mask_image = cv2.imdecode(mask_numpy, cv2.IMREAD_GRAYSCALE)
+            mask_binary = (mask_image > 127).astype(np.uint8)
 
-            mask_binary = (mask_img > 127).astype(np.uint8)
-
-            # Compute 3D center
+            # Compute center
             center = self.mask_to_center_3d(mask_binary)
             if center is not None:
                 block_centers.append(center)
@@ -105,10 +103,12 @@ class BlockDetection(Node):
             # Visualization
             color = np.random.randint(0, 255, size=3)
             mask_rgb = np.dstack([mask_binary * color[i] for i in range(3)])
-            all_masks = cv2.addWeighted(all_masks, 1.0, mask_rgb, 0.6, 0)
+            masks_stacked = cv2.addWeighted(masks_stacked, 1.0, mask_rgb, 0.6, 0)
 
-        self.publish_masks(all_masks)
+        # Publishers
+        self.publish_masks(masks_stacked)
         self.publish_markers(block_centers)
+        print(f"Published {len(sam_data)} masks to /binary_masks")
 
 
     def mask_to_center_3d(self, mask):
@@ -122,6 +122,7 @@ class BlockDetection(Node):
             return None
 
         xs, ys, Z = xs[valid], ys[valid], Z[valid]
+        Z *= self.depth_scale
 
         X = (xs - self.cx) * Z / self.fx
         Y = (ys - self.cy) * Z / self.fy
@@ -134,35 +135,38 @@ class BlockDetection(Node):
         msg = self.bridge.cv2_to_imgmsg(masks, "bgr8")
         msg.header.frame_id = "camera_color_optical_frame"
         msg.header.stamp = self.get_clock().now().to_msg()
-        self.mask_pub.publish(msg)
+        self.binary_masks_pub.publish(msg)
+
 
     def publish_markers(self, centers):
         marker_array = MarkerArray()
 
-        for i, c in enumerate(centers):
-            m = Marker()
-            m.header.frame_id = "camera_color_optical_frame"
-            m.header.stamp = self.get_clock().now().to_msg()
-            m.ns = "blocks"
-            m.id = i
-            m.type = Marker.SPHERE
-            m.action = Marker.ADD
+        for id, center in enumerate(centers):
+            marker = Marker()
+            marker.header.frame_id = "camera_color_optical_frame"
+            marker.header.stamp = self.get_clock().now().to_msg()
 
-            m.pose.position.x = float(c[0]) * self.depth_scale
-            m.pose.position.y = float(c[1]) * self.depth_scale
-            m.pose.position.z = float(c[2]) * self.depth_scale
-            m.pose.orientation.w = 1.0
+            marker.id = id
+            marker.ns = "blocks"
+            marker.type = Marker.SPHERE
+            marker.action = Marker.ADD
 
-            m.scale.x = m.scale.y = m.scale.z = 0.03
-            m.color.r = 1.0
-            m.color.g = 0.0
-            m.color.b = 0.0
-            m.color.a = 1.0
+            marker.pose.position.x = float(center[0])
+            marker.pose.position.y = float(center[1])
+            marker.pose.position.z = float(center[2])
+            marker.pose.orientation.w = 1.0
 
-            m.lifetime.sec = int(self.timer_period)
-            marker_array.markers.append(m)
+            marker.scale.x = marker.scale.y = marker.scale.z = 0.03
+            marker.color.r = 1.0
+            marker.color.g = 0.0
+            marker.color.b = 0.0
+            marker.color.a = 1.0
 
-        self.marker_pub.publish(marker_array)
+            marker.lifetime.sec = int(self.detection_period)
+            marker_array.markers.append(marker)
+
+        self.block_centers_pub.publish(marker_array)
+        print(f"Published {len(marker_array)} centers to /block_centers")
 
 
 def main(args=None):
@@ -171,7 +175,6 @@ def main(args=None):
     rclpy.spin(node)
     node.destroy_node()
     rclpy.shutdown()
-
 
 if __name__ == "__main__":
     main()
